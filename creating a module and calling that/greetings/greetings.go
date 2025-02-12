9b1a641697b0ae19fe121da4450b5959eaf3a272
prathamisonline <prathamsrivastavas@gmail.com>
2025-02-12 10:21:31 +0530

    created a multiple return function

--- creating a module and calling that/greetings/greetings.go
+++ creating a module and calling that/greetings/greetings.go
@@ -31,3 +31,21 @@ func randomFormat() string {
 	// Return a randomly selected message format by specifying a random index for the slice of formats.
 	return formats[rand.Intn(len(formats))]
 }
+
+// Hello retuns a map that asscoicates each of the named people with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map o associate names with messages.
+	messages := make(map[string]string)
+
+	// Loop through the received slice of names, calling the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+		// In the map, associate the retrieved message with the name.
+		messages[name] = message
+	}
+	return messages, nil
+}
--- creating a module and calling that/hello/hello.go
+++ creating a module and calling that/hello/hello.go
@@ -12,14 +12,14 @@ func main() {
 	// the log entry prefix and a flag to disable printing the time, source file, and line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-
+	names := []string{"Pratham", "Raj", "Rahul"}
 	// request a greeting message.
-	message, err := greetings.Hello("Pratham")
+	messages, err := greetings.Hellos(names)
 	// if an error was returned, print it to the console and exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
 	// if no error was returned, print the returned message to the console.
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
