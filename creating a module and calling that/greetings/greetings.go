b34b7322767ee2e56024b3cc69c48be6ace44109
prathamisonline <prathamsrivastavas@gmail.com>
2025-02-12 10:07:05 +0530

    creating a random function to create multiple random greetings

--- creating a module and calling that/greetings/greetings.go
+++ creating a module and calling that/greetings/greetings.go
@@ -1,8 +1,33 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	// if no name was given, return an error with a message
+	if name == "" {
+		return name, errors.New("empyt name")
+	}
+	// creating a mesage using a random format
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+
+}
+
+// randomFormat returns one of a set of greeting messages. The returned message is selected at random.
+// message is seleced at random
+
+func randomFormat() string {
+	// A slice of message formats.
+
+	formats := []string{
+		"hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	// Return a randomly selected message format by specifying a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
--- creating a module and calling that/hello/hello.go
+++ creating a module and calling that/hello/hello.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Pratham")
+	// set properties of the predefined Logger, including the log entry prefix and a flag to disable printing the time, source file, and line number.
+	// the log entry prefix and a flag to disable printing the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// request a greeting message.
+	message, err := greetings.Hello("Pratham")
+	// if an error was returned, print it to the console and exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// if no error was returned, print the returned message to the console.
+
 	fmt.Println(message)
 }
