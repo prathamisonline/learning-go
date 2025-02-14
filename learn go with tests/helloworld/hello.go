1e4dc66d62155954c4ac42d2c7eab70b757e2828
prathamisonline <prathamsrivastavas@gmail.com>
2025-02-14 16:54:43 +0530

    added multiple test for saying hello in other lang

--- learn go with tests/helloworld/hello.go
+++ learn go with tests/helloworld/hello.go
@@ -4,14 +4,17 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == "Spanish" {
+		return "Hola, " + name
 	}
 	return englishHelloPrefix + name
 
 }
 
 func main() {
-	fmt.Println(Hello("Pratham"))
+	fmt.Println(Hello("Pratham", "Spanish"))
 }
--- learn go with tests/helloworld/hello_test.go
+++ learn go with tests/helloworld/hello_test.go
@@ -4,13 +4,18 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Pratham")
+		got := Hello("Pratham", "")
 		want := "Hello, Pratham"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Saying hello in other languages", func(t *testing.T) {
+		got := Hello("Pratham", "Spanish")
+		want := "Hola, Pratham"
 		assertCorrectMessage(t, got, want)
 	})
 }
